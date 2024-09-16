cacbc34d9236926e18b82cdd610d733ce14e80c2
irades <irad3s@gmail.com>
2024-09-16 18:52:14 +0300

    add solutions to tasks 1-4

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
+Write a program to calculate the body mass index (BMI) of a person. 
+The user must enter their height and weight, 
+after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
 weight = input()
 height = input()
 
-
-#code here
-
+weight = int(weight)
+height = int(height)
+bmi = weight / (height ** 2)
+print(bmi)
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Develop a program that asks the user for an integer 4-digit number and calculates the sum of its constituent digits. For example, if the user enters the number 3141, the program should output the following result:
+Develop a program that asks the user for an integer 4-digit number 
+and calculates the sum of its constituent digits. 
+For example, if the user enters the number 3141, 
+the program should output the following result:
 3 + 1 + 4 + 1 = 9
 
 """
+
+def sum_of_digits():
+    try:
+        user_input = input("Enter a 4-digit number:\n")
+        if not user_input.isdigit() or len(user_input) != 4:
+            raise ValueError("Your input must be a 4-digit number.")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    else:
+        # a = user_input[0]
+        # b = user_input[1]
+        # c = user_input[2]
+        # d = user_input[3]
+        a, b, c, d = user_input
+        resulting_sum = sum(int(x) for x in user_input)
+        answer = f"{a} + {b} + {c} + {d} = {resulting_sum}"
+        print(answer)
+
+sum_of_digits()
